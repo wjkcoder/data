2e38a12bb6a92c25c207a3ae17564bcaf52f7a04
wonder <wonder@charsmart.com>
2022-01-05 16:03:32 +0800

    update

--- src/main/java/com/charsmart/data/controller/BizController.java
+++ src/main/java/com/charsmart/data/controller/BizController.java
@@ -3,16 +3,13 @@ package com.charsmart.data.controller;
 import com.charsmart.data.common.Result;
 import com.charsmart.data.domain.DomainService;
 import com.charsmart.data.domain.ServiceA;
-import com.charsmart.data.domain.ServiceB;
 import com.charsmart.data.domain.impl.DomainServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -24,11 +21,9 @@ public class BizController {
     private final DomainService service;
     @Autowired
     public List<ServiceA> serviceAs;
-    private final ServiceB serviceB;
 
-    public BizController(DomainServiceImpl service, ServiceB serviceB) {
+    public BizController(DomainServiceImpl service) {
         this.service = service;
-        this.serviceB = serviceB;
     }
 
     @RequestMapping("/user-info")
